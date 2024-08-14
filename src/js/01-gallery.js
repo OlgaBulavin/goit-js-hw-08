335861304f59b82ee8eaaa5b1155dcf78598d7fa
Olga Bulavin <olga.bulavin89@gmail.com>
2024-08-14 11:27:39 +0200

    hw-js-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+  new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionSelector: 'img',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+import throttle from 'lodash.throttle'
+import '../css/common.css';
+import '../css/03-feedback.css';
+
+
+const STORAGE_KEY = 'feedback-form-state';
+const submit = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
+const formData = {};
+
+const refs = {
+    form:document.querySelector('.js-feedback-form'),
+    textarea: document.querySelector('.js-feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput), 500);
+
+refs.form.addEventListener('input', evt => {
+    formData[evt.target.email] = evt.target.message;
+})
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY, JSON.stringify(submit));
+}
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    const stringData = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, stringData);
+   
+}
+
+function populateTextarea() {
+    const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage) {
+        refs.textarea.value = saveMessage;
+    }
+}
