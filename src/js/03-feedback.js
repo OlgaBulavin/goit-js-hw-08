f08647e415191b356a19e24884d64f4256ccbf2e
Olga Bulavin <olga.bulavin89@gmail.com>
2024-08-25 14:18:29 +0200

    js8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,35 +7,33 @@ import '../css/03-feedback.css';
 const STORAGE_KEY = 'feedback-form-state';
 const submit = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
 
-const formData = {
-  email: '',
-  message: '',
-};
-
 
 const form = document.querySelector('.js-feedback-form');
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onInputData), 500);
 
-refs.form.addEventListener('input', evt => {
-    formData[evt.target.email] = evt.target.message;
-})
-populateTextarea();
+populateForm();
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    evt.currentTarget.reset();
+
+    const {
+        elements: {email, message},
+    } = evt.currentTarget;
+    
+  const result = {email: email.value, message: message.value};
+   evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY, JSON.stringify(submit));
 }
 
-function onTextareaInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    const message = JSON.stringify(formData);
+function onFormInput(evt) {
+    const message = evt.currentTarget.value;
+ 
     localStorage.setItem(STORAGE_KEY, message);
 }
-
-function populateTextarea() {
+ 
+function populateForm() {
     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (savedMessage) {
         refs.form.value = savedMessage;
