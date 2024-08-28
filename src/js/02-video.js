16a6b89766bc094c39a0b698a8cf309e1129ae22
Olga Bulavin <olga.bulavin89@gmail.com>
2024-08-28 19:34:56 +0200

    js-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+
+    function onPlay({seconds}) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+    }
+
+
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,40 +3,39 @@ import throttle from 'lodash.throttle'
 import '../css/common.css';
 import '../css/03-feedback.css';
 
-
 const STORAGE_KEY = 'feedback-form-state';
-const submit = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
 
 
 const form = document.querySelector('.js-feedback-form');
+const textarea = document.querySelector('.js-feedback-form');
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onInputData), 500);
+textarea.addEventListener('input', onTextareaInput);
 
-populateForm();
+populateTextarea();
 
-function onFormSubmit(evt) {
+form.elements.message.value = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? "";
+
+form.addEventListener("input", (evt) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(evt.target.value));
+})
+
+form.addEventListener("submit", (evt) => {
     evt.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+});
 
-    const {
-        elements: {email, message},
-    } = evt.currentTarget;
-    
-  const result = {email: email.value, message: message.value};
-   evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY, JSON.stringify(submit));
-}
-
-function onFormInput(evt) {
-    const message = evt.currentTarget.value;
- 
-    localStorage.setItem(STORAGE_KEY, message);
-}
- 
-function populateForm() {
+function populateTextarea() {
     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (savedMessage) {
-        refs.form.value = savedMessage;
+        textarea.value = savedMessage;
     }
 }
 
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
